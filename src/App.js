e3acf1fddc3e686a6fb052a481f3f2dd39838ecf
prostochlen <iwan.maksackov@yandex.ru>
2020-10-17 12:04:10 +0300

    second day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+        <>
+        <Header />
+        <Main />
+        <Features />
+        <Footer />
+        </>
+      );
+  }
 }
 
 export default App;
